0ea4197073a2f0a2d84454db772e786732a18c5e
Ryan Zidago <ryanzidago@gmail.com>
2021-05-12 20:06:18 +0200

    wip

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,21 @@
 class Serializer
+  # class << self
+  #   alias_method :attribute, :attr_accessor
+  # end
+
+  def initialize(body)
+    @id = 1
+    @body = body
+  end
+
+  def attribute(attr)
+    attr
+  end
+
+  def serialize
+    {
+      id: @id,
+      body: @body,
+    }
+  end
 end
