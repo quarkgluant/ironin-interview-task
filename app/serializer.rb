39ee22c81ece1bf209d12260152b8294a79c08bc
Patrice Cadiot <pathibul.r@gmail.com>
2021-05-15 19:57:12 +0200

    always cheating, but removes debug comments and uses yield

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,48 +1,10 @@
-# class Serializer
-  # # class << self
-  # #   alias_method :attribute, :attr_accessor
-  # # end
-  # # attr_reader :object
-  # @@attributes = []
-  # def initialize(struct)
-  #   @struct = struct
-  #   @klass = struct.class
-  #   p @klass
-  #   @@attributes << @klass
-  #   p @@attributes
-  #   struct.members.each do |member|
-  #     define_singleton_method member do
-  #       struct.dig member
-  #     end
-  #   end
-  #   # define_singleton_method :object do
-  #   #   struct.class
-  #   # end
-  #   # @id = struct[:id]
-  #   # @body = struct[:body]
-  # end
-  #
-  #
-  #
-  # def self.attribute(attr)
-  #   if block_given?
-  #
-  #   else
-  #     @@attributes.first.dig(attr)
-  #   end
-  # end
-  #
-  # def serialize
-  #   @struct.to_h
-  # end
 class Serializer
   attr_reader :struct
   @@fields = []
 
   def initialize(struct)
-
     @struct = struct
-    # je crée des accesseurs pour chaque membre
+    # je crée des accesseurs pour chaque membre, pour pouvoir faire Post.date ou Comment.id
     puts "struct.class: #{struct.class} "
     struct.members.each do |member|
       struct.class.define_singleton_method member do
@@ -52,7 +14,7 @@ class Serializer
     puts "############## #{struct.class}.methods: #{struct.class.methods(false)}  ############################"
   end
 
-  def self.attribute(attribute_name, &block)
+  def self.attribute(attribute_name)
     # je récupère le nom de la classe sans le suffixe 'Serializer'
     klass_name = self.to_s.delete_suffix('Serializer')
     puts "-------------- self.to_s.delete_suffix('Serializer') #{self.to_s.delete_suffix('Serializer')} -----"
@@ -61,12 +23,7 @@ class Serializer
     puts "############## #{object.class}.methods: #{object.class.methods(false)}  ############################"
     puts "############## #{object}.methods: #{object.methods(false)}  ############################"
 
-    # puts "############## object.class.members #{object.class.members}  ############################"
-    # result = object.send attribute_name
-    # # result = object.dig("#{attribute_name}")
-    # puts "+++++++++++ #{result}"
-    # result = yield object if block_given?
-    my_proc = proc { |object| block.call(object) } if block
+    my_proc = proc { |object| yield object } if block_given?
     @@fields << { self.to_s  => [attribute_name, object, my_proc] }
   end
 
@@ -76,10 +33,8 @@ class Serializer
       hash_field.keys.each do |klass_name|
         if self.class.name == klass_name.to_s
           attr, object, my_proc = hash_field[klass_name]
-          puts "#{my_proc}.is_a?(Proc) : #{my_proc.is_a?(Proc)} "
           result = my_proc.call(object) if my_proc.is_a?(Proc)
           result = object.send attr unless my_proc.is_a?(Proc)
-          puts "result: #{result}"
           hash[attr] = result
         end
       end
