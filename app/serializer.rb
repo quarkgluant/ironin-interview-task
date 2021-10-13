c42350dce4b74b46645c15200a3c64fa0f03663d
Patrice Cadiot <pathibul.r@gmail.com>
2021-10-13 19:33:28 +0200

    Removes code

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,20 +1,2 @@
 class Serializer
-  attr_reader :object
-  @@fields = Hash.new { |h, k| h[k] = {} }
-
-  def initialize(struct)
-    @object = struct
-  end
-
-  def self.attribute(attribute_name, &attribute_bloc)
-     @@fields[self.to_s][attribute_name] = attribute_bloc
-  end
-
-  def serialize
-    @@fields[klass_name = self.class.to_s].keys.each_with_object({}) do |attribute_name, result|
-      my_proc = @@fields[klass_name][attribute_name]
-      value = my_proc.is_a?(Proc) ? instance_eval(&my_proc) : object.send(attribute_name)
-      result[attribute_name] = value
-    end
-  end
 end
